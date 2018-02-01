105a10c2af2b3afd488e28f0c41dada370a98e1f
JC <johncedricktee@gmail.com>
2018-02-01 15:15:14 -0800

    added juliet service controller

---
+++ application/controllers/Juliet.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Juliet Controller, returns JSON data
+ *
+ * @author JC
+ */
+class Juliet extends CI_Controller {
+    function __construct()
+	{
+		parent::__construct();
+	}
+
+	
+	public function index()
+	{
+            $record = $this->quotes->get(1);
+            header("Content-type: application/json");
+            echo json_encode($record);
+	}
+}
