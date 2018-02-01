66da4cd652483a4df78b82c2559beffc5ff06aee
Tliyen <liyan.tong@gmail.com>
2018-02-01 14:18:42 -0800

    Create Tango.php and add function to show 5th actor position in foxtrot index

---
+++ application/controllers/Tango.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Tango extends Application
+{
+
+	/**
+	 * Function shows the 5th position actor.  This is routed to Foxtrot
+   * that invokes showing the information with the key "5"
+	 */
+	public function index()
+	{
+    $this->show(5);
+	}
+
+}
